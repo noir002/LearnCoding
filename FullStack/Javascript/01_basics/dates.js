b1954db282db1b63527a9880a85c4ae6e6c11ffd
noir@002 <paras.chauhan.93893@gmail.com>
2025-07-11 20:14:08 +0530

    JS folder updated

---
+++ FullStack/Javascript/01_basics/dates.js
@@ -0,0 +1,15 @@
+// Dates
+
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toUTCString());
+
+let createMyDate = new Date(2025,0,23)
+let createMyDate1 = new Date(2023,1,24)
+console.log(createMyDate.toLocaleString());
+console.log(createMyDate1.toLocaleString());
