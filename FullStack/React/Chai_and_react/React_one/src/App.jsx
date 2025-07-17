f739c3d91d7f6426cd4800afd6271c1aae623dac
noir@002 <paras.chauhan.93893@gmail.com>
2025-07-17 22:46:15 +0530

    CPP Files added

--- FullStack/React/Chai_and_react/React_one/src/App.jsx
+++ FullStack/React/Chai_and_react/React_one/src/App.jsx
@@ -4,10 +4,10 @@ function App() {
  
 
   return (
-    <>
-      <h1>This is a line</h1>
+    <div className="flex-col flex justify-center items-center bg-gray-700 h-screen">   // Use className instead of class
+      <h1 >This is a line</h1>
       <h2>You are reading english</h2>
-    </>
+    </div>
   )
 }
 
